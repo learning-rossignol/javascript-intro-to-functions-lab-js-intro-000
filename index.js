9ddb8ca6736ef55bb850ea9bdd2a47c608c968ae
Jean-François Rompré <jrompre@gmail.com>
2019-10-10 22:30:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ function whisper(string) {
 function logShout(s) {
   console.log( shout(s))
 }
+
+function logWhisper(s) {
+  console.log( whisper(s))
+}
+
+function sayHiToGrandma (in) {
+  var upperIn = in.toUpperCase();
+  var lowerIn = in.toLowerCase();
+  var scream = "YES INDEED!";
+  var canthear = "I can\'t hear you!";
+  var loveydovey = "I love you, Grandma.";
+  var lovya = "I love you, too.";
+  if (in === upperIn) {
+    return scream;
+  }
+  else if (in === lowerIn) {
+    return canthear;
+  }
+  else if (in === )
+}
